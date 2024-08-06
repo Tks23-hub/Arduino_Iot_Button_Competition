e08afcde6e202ad5e2c8591e333f875d17e41521
Tks23-hub <128156006+Tks23-hub@users.noreply.github.com>
2024-08-06 14:17:28 +0300

    Creating SendData Function
    The send data function that will send the shortest Button Click to the server

--- K20_Remote_Wifi.ino
+++ K20_Remote_Wifi.ino
@@ -19,3 +19,18 @@ void wifiClient_Setup() {
     Serial.println("Connected to network");
  }
    
+  void SendData(int val) {
+  String dataURL = "http://api.kits4.me/GEN/api.php?ACT=SET&DEV=1121&CH=1&VAL=" + String(val);
+  Serial.println("Full URL: " + dataURL);
+  HTTPClient http;
+  http.begin(client, dataURL);
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    Serial.println("HTTP Code: " + String(httpCode)); 
+  } else {
+    Serial.println("HTTP request failed, Send: " + String(httpCode));
+  }
+  
+  http.end();
+}
+ 
